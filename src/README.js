f5d801cfaccefa2dd67903c2aa110a53123a0a3d
Pc-Akkaracha <akkaracha555123@gmail.com>
2024-08-25 00:23:21 +0700

    POST

--- src/README.js
+++ src/README.js
@@ -1,4 +1,4 @@
-Solution
+/* Solution
 s = รวมตัวเลข
 n = รวมตัวเลข
 index = รอบต่อไปที่นำไปคิด index จะเกิน
@@ -9,11 +9,11 @@ target = 3
 
 // start
 findCombine(0, 0, [])
-s findCombine(1, 1, [1])
-s findCombine(2, 3, [1,2]) target (!stop)
-n findCombine(2, 1, [1])
-s findCombine(3, 4, [1,3]) index
-n findCombine(3, 1, [1]) index
+    s findCombine(1, 1, [1])
+        s findCombine(2, 3, [1,2]) target (!stop)
+        n findCombine(2, 1, [1])
+            s findCombine(3, 4, [1,3]) index
+            n findCombine(3, 1, [1]) index
 
     n findCombine(1, 0, []);
         s findCombine(2, 2, [2])
@@ -22,3 +22,5 @@ n findCombine(3, 1, [1]) index
         n findCombine(2, 0, [])
             s findCombine(3, 3, [3]) target (!stop)
             n findCombine(3, 0, []) index
+
+*/
